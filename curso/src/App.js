20e45b0bacb6915585df1aee93b0a5f542bbf46c
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-02-26 14:14:52 -0300

    Initial Commit

---
+++ curso/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import Primeiro from './components/Primeiro';
+
+function App() {
+  return (
+    <Primeiro nome="Primeiro Component" />
+  );
+}
+
+export default App;
---
+++ curso/src/components/Primeiro.js
@@ -0,0 +1,10 @@
+
+import './Primeiro.css';
+
+function Primeiro(props) {
+  return (
+    <h1>{props.nome}</h1>
+  );
+}
+
+export default Primeiro;
